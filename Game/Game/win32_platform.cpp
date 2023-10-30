2f0c6ea049eb50beebfe7e321ed599a0ab95805c
Viktoria <vikibibetko@gmail.com>
2023-10-31 00:22:18 +0100

    Widow created

--- Game/Game/win32_platform.cpp
+++ Game/Game/win32_platform.cpp
@@ -1,19 +1,46 @@
 #include <windows.h>
 
+bool running = true;
+
 LRESULT CALLBACK WindowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+
+	LRESULT result = 0;
+	switch (uMsg)
+		{
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+		} break;
+
+		default:
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+		return result;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-    
-    WNDCLASS windowClass = {};
-    windowClass.style = CS_HREDRAW | CS_VREDRAW;
-    windowClass.lpfnWndProc = WindowCallback;
-    windowClass.hInstance = hInstance; 
-    windowClass.lpszClassName = "Game Window lass";
+
+	WNDCLASS windowClass = {};
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = WindowCallback;
+	windowClass.hInstance = hInstance;
+	windowClass.lpszClassName = "Game Window lass";
 
 
-    RegisterClass(&windowClass);
+	RegisterClass(&windowClass);
 
-    CreateWindow(windowClass.lpszClassName, "Cpp Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	CreateWindow(
+		windowClass.lpszClassName,
+		"Cpp Game",
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		1280, 720,
+		0, 0,
+		hInstance,
+		0);
+
+	while (running) {
+
+	}
+}
 
