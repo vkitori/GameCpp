33c3ab676517e84c601ff1ed3d6f51ea9a12f96a
Viktoria <vikibibetko@gmail.com>
2023-11-01 20:00:29 +0100

    windows params defined

--- Game/Game/win32_platform.cpp
+++ Game/Game/win32_platform.cpp
@@ -2,6 +2,11 @@
 
 bool running = true;
 
+void* buffer_memory;
+int buffer_width;
+int buffer_height;
+BITMAPINFO buffer_bitmap_info;
+
 LRESULT CALLBACK WindowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 
 	LRESULT result = 0;
@@ -10,6 +15,28 @@ LRESULT CALLBACK WindowCallback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 		case WM_CLOSE:
 		case WM_DESTROY: {
 			running = false;
+		} break;
+
+		case WM_SIZE: {
+			RECT rect;
+			GetClientRect(hwnd, &rect);
+			buffer_width = rect.right - rect.left;
+			buffer_height = rect.bottom - rect.top;
+
+			int buffer_size = buffer_width * buffer_height * sizeof(unsigned int);
+
+			if (buffer_memory) VirtualFree(buffer_memory, 0, MEM_RELEASE);
+			buffer_memory = VirtualAlloc(0, buffer_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			buffer_bitmap_info.bmiHeader.biSize = sizeof(buffer_bitmap_info.bmiHeader);
+			buffer_bitmap_info.bmiHeader.biWidth = buffer_width;
+			buffer_bitmap_info.bmiHeader.biHeight = buffer_height;
+			buffer_bitmap_info.bmiHeader.biPlanes = 1;
+			buffer_bitmap_info.bmiHeader.biBitCount = 32;
+			buffer_bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+
+
 		} break;
 
 		default:
@@ -24,12 +51,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	windowClass.style = CS_HREDRAW | CS_VREDRAW;
 	windowClass.lpfnWndProc = WindowCallback;
 	windowClass.hInstance = hInstance;
-	windowClass.lpszClassName = "Game Window lass";
+	windowClass.lpszClassName = "Game Window Class";
 
 
 	RegisterClass(&windowClass);
 
-	CreateWindow(
+	HWND window = CreateWindow(
 		windowClass.lpszClassName,
 		"Cpp Game",
 		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
@@ -38,9 +65,23 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		0, 0,
 		hInstance,
 		0);
+	HDC hdc = GetDC(window);
 
 	while (running) {
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+		
+		unsigned int* pixel = (unsigned int*)buffer_memory;
+		for (int y = 0; y < buffer_height; y++) {
+			for (int x = 0; x < buffer_width; x++) {
+				*pixel++ = 0x00CED1;
+			}
+		}
 
+		StretchDIBits(hdc, 0, 0, buffer_width, buffer_height, 0, 0, buffer_width, buffer_height, buffer_memory, &buffer_bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 	}
 }
 
